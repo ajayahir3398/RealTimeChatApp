b5af3a138cf854b2104e175d4c855ae1d4dd004e
Ajay Bandhiya <109127690+ajayahir3398@users.noreply.github.com>
2025-07-13 16:53:00 +0530

    Update swagger.js

--- config/swagger.js
+++ config/swagger.js
@@ -18,7 +18,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: 'https://chat-app-api-111k.onrender.com',
         description: 'Development server'
       }
     ],
